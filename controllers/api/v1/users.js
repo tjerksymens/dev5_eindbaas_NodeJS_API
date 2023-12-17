1e9e4aa8fdfd15983ce00410f53a230acc3073bf
Tjerk Symens <98127222+tjerksymens@users.noreply.github.com>
2023-12-17 16:27:22 +0100

    feat: change login with jwt token

--- controllers/api/v1/users.js
+++ controllers/api/v1/users.js
@@ -39,10 +39,21 @@ const signup = async (req, res ,next) => {
 // POST /api/v1/users/login
 const login = async (req, res, next) => {
     const user = await User.authenticate()(req.body.username, req.body.password).then(result => {
-        res.json({
+        if(!result.user){
+            return res.json({
+                status: "failed",
+                message: "Login failed"
+            });
+        }
+
+        let token = jwt.sign({
+            uid: result.user._id,
+        }, "MyVerySecretWord");
+        
+        return res.json({
             status: "success",
             data: {
-                user: result,
+                token: token,
             }
         });
     }).catch(error => {
