20bdbe90d62464914085105b526c66f3760a9ac3
Tjerk Symens <98127222+tjerksymens@users.noreply.github.com>
2023-12-17 23:46:59 +0100

    fix: use cors

--- routes/api/v1/shoes.js
+++ routes/api/v1/shoes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("../../../passport/passport");
+const cors = require("cors");
 
 // import controller
 const shoesController = require("../../../controllers/api/v1/shoes");
 
+router.use(cors());
 router.get("/", passport.authenticate('jwt', { session: false }), shoesController.index);
 router.delete("/:id", passport.authenticate('jwt', { session: false }), shoesController.cancel);
 router.patch("/:id", shoesController.orderStatus);
--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("../../../passport/passport");
+const cors = require("cors");
 
 // import controller
 const usersController = require("../../../controllers/api/v1/users");
 
+router.use(cors());
 router.post('/signup', usersController.signup);
 router.post('/login', usersController.login);
 router.post('/change-password', usersController.changePassword);
